4747122115a7e1031b1cc709c9e4eb646a0f805e
RobyLedda94 <robyledda01@yahoo.it>
2024-09-26 16:55:07 +0200

    [UPD] creato il modello, creato il seeder e popolato i record della tabella con dati fake, ok li mostra in phpmyadmin

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+// importo il faker generator
+use Faker\Generator as Faker; 
+// utilizzo il modello train
+use App\Models\Train;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -12,8 +16,23 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=0; $i<10; $i++){
+            $train = new Train();
+            // popolo la tabella con dati faker
+            $train->company = $faker->company();
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure_time = $faker->time();
+            $train->arrival_time = $faker->time();
+            $train->train_code = $faker->randomNumber(5, true);
+            $train->carriage_number = $faker->numberBetween(2, 12);
+            $train->on_time = $faker->boolean();
+            $train->cancelled = $faker->boolean();
+
+            // salvo i dati nei record
+            $train->save();
+        }
     }
 }
