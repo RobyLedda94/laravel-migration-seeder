922626e08f0c2dc85b643ee91f4662134248b69d
RobyLedda94 <robyledda01@yahoo.it>
2024-09-26 15:59:14 +0200

    [UPD] creata la struttura della tabella trains, lanciato il comando php artisan migrate, ok tabella in phpmyadmin

--- database/migrations/2024_09_26_132641_create_trains_table.php
+++ database/migrations/2024_09_26_132641_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 100);  // azienda
+            $table->string('departure_station', 200); // stazione di partenza
+            $table->string('arrival_station', 200); // stazione di partenza
+            $table->time('departure_time'); // orario di partenza
+            $table->time('arrival_time'); // orario d'arrivo
+            $table->string('train_code', 20); // codice treno
+            $table->tinyInteger('carriage_number'); // numero di carrozze
+            $table->boolean('on_time'); // indica se il treno è in orario
+            $table->boolean('cancelled'); // indica se il treno è cancellato
             $table->timestamps();
         });
     }
